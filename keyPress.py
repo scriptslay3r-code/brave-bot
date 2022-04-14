98e45b5cdc445943b34b0214a7b5b3f9ade13fab
scriptslay3r-code <Jackrabbitaltima@gmail.com>
2022-04-13 22:08:45 -0500

    update keyPress.py

--- keyPress.py
+++ keyPress.py
@@ -1,6 +1,6 @@
 from pyKey import pressKey, releaseKey, press, sendSequence, showKeys
 #https://github.com/andohuman/pyKey
 
-pressKey("Control_L")
+pressKey("LCTRL")
 pressKey("Alt_L")
 pressKey("Tab")
