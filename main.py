5df037f41d1078a004f997c758821bf0bc915c4a
scriptslay3r-code <Jackrabbitaltima@gmail.com>
2022-04-14 08:51:18 -0500

    update main.py

--- main.py
+++ main.py
@@ -38,8 +38,8 @@ def intro():
 
 intro()
 driver.get(url)
-buttonSearch = driver.find_element_by_id("search_button_homepage")
-searchBox = driver.find_element_by_id("search_form_input_homepage")
+buttonSearch = driver.find_element(By.ID,"search_button_homepage")
+searchBox = driver.find_element(By.ID"search_form_input_homepage")
 driver.execute_script("return arguments[0].scrollIntoView();", searchBox)
 time.sleep(1)
 searchBox.click()
